d695128274a288ded8cf27b28d034facc32256c5
Andrey-Mkoyan <and.mkoyan07@gmail.com>
2021-12-23 16:52:58 +0400

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/footer/footer.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { NetworkComponent } from './pages/network/network.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { NoticesComponent } from './pages/notices/notices.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'feed', component: FeedComponent },
+  { path: 'network', component: NetworkComponent },
+  { path: 'jobs', component: JobsComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: 'notices', component: NoticesComponent },
+
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    FeedComponent,
+    NetworkComponent,
+    JobsComponent,
+    ChatComponent,
+    NoticesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+
+}
